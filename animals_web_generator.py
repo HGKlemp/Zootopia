74cbaaa633afe25bd669de1d331ee6bf19feb36e
Guenter Klemp <g.klemp@gmx.de>
2026-06-18 11:10:58 +0200

    Showed all animals in animals.html. Insert (<meta charset='UTF-8'>) to the template to show the ' correctly !

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -8,7 +8,9 @@ with open("animals_data.json", "r") as handle:
 with open("animals_template.html", "r") as handle:
     html_template = handle.read()
 
-animals_text = ""
+
+animals_html = ""
+
 
 for animal in animals_data:
 
@@ -18,20 +20,27 @@ for animal in animals_data:
     locations = animal.get("locations", [])
     location = locations[0] if locations else "Unknown"
 
-    animal_type = animal.get("characteristics", {}).get("type", "")
+    animal_type = animal.get("characteristics", {}).get("type")
 
+    animals_html += "<li class='cards__item'>\n"
 
-    animals_text += f"Name: {name} "
-    animals_text += f"Diet: {diet} "
-    animals_text += f"Location: {location}"
+    animals_html += f"  <h2 class='card__title'>{name}</h2>\n"
+    animals_html += "  <div class='card__text'>\n"
+
+    animals_html += f"    <p><strong>Diet:</strong> {diet}</p>\n"
+    animals_html += f"    <p><strong>Location:</strong> {location}</p>\n"
 
     if animal_type:
-        animals_text += f" Type: {animal_type}"
+        animals_html += f"    <p><strong>Type:</strong> {animal_type}</p>\n"
 
-    animals_text += "\n"
+    animals_html += "  </div>\n"
+    animals_html += "</li>\n"
 
 
-new_html = html_template.replace("__REPLACE_ANIMALS_INFO__", animals_text)
+new_html = html_template.replace(
+    "__REPLACE_ANIMALS_INFO__",
+    animals_html
+)
 
 
 with open("animals.html", "w") as handle:
